91b3ce78f9541820c4dfa9df5052794c57a3e4cf
Rolando Solano <rsolano@gmail.com>
2020-05-05 21:14:17 -0600

    AdvertApi.SearchWorker first commit

---
+++ WebAdvert.SearchWorker/SearchWorker.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+using System;
+
+[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace WebAdvert.SearchWorker
+{
+    public class SearchWorker
+    {
+        public void Function(SNSEvent snsEvent, ILambdaContext context)
+        {
+            foreach (var record in snsEvent.Records)
+            {
+                context.Logger.LogLine(record.Sns.Message);
+            }
+        }
+    }
+}
